35efae38465465d68bf34ea44a4f7e053f7436a3
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-03-14 21:46:12 +0300

    made new controllers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,15 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { PrismaService } from './prisma.service'
 import { UserModule } from './user/user.module'
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { PaginationModule } from './pagination/pagination.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, ReviewModule, CategoryModule, OrderModule, StatisticsModule, PaginationModule],
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
 })
---
+++ backend/src/statistics/statistics.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { StatisticsService } from './statistics.service';
+
+@Controller('statistics')
+export class StatisticsController {
+  constructor(private readonly statisticsService: StatisticsService) {}
+}
