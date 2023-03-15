07d936eb03a7b52de9345c0f764da86f1db72bf3
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-03-16 02:08:52 +0300

    made review.service logic

--- frontend/src/services/category.service.ts
+++ frontend/src/services/category.service.ts
@@ -1,45 +1,41 @@
-import Cookies from "js-cookie"
-import axios from 'axios'
-import { getContentType } from "../api/api.helper"
-import { saveToStorage } from "./auth/auth.helper"
 import { instance } from "../api/api.interceptor"
 import { ICategory } from "../types/category.interface"
 
-const CATEGORY = 'category'
+const CATEGORIES = 'categories'
 
 export const CategoryService = {
 
     async getAll() {
         return await instance<ICategory[]>({
-            url: CATEGORY,
+            url: CATEGORIES,
             method: 'GET'
         })
     },
 
     async getById(id: string | number) {
         return await instance<ICategory>({
-            url: `${CATEGORY}/${id}`,
+            url: `${CATEGORIES}/${id}`,
             method: 'GET'
         })
     },
 
     async getBySlug(slug: string) {
         return await instance<ICategory>({
-            url: `${CATEGORY}/by-slug/${slug}`,
+            url: `${CATEGORIES}/by-slug/${slug}`,
             method: 'GET'
         })
     },
 
     async createCategory() {
         return await instance<ICategory>({
-            url: CATEGORY,
+            url: CATEGORIES,
             method: 'POST'
         })
     },
 
     async updateCategory(id: string | number, name: string) {
         return await instance<ICategory>({
-            url: `${CATEGORY}/${id}`,
+            url: `${CATEGORIES}/${id}`,
             method: 'PUT',
             data: { name }
         })
@@ -47,7 +43,7 @@ export const CategoryService = {
 
     async deleteCategory(id: string | number) {
         return await instance<ICategory>({
-            url: `${CATEGORY}/${id}`,
+            url: `${CATEGORIES}/${id}`,
             method: 'DELETE'
         })
     },
---
+++ frontend/src/services/review.srevice.ts
@@ -0,0 +1,28 @@
+import { instance } from "../api/api.interceptor"
+import { IReview } from "../types/review.interface"
+
+const REVIEWS = 'reviews'
+
+type DataType = {
+    rating: number
+    text: string
+}
+
+export const ReviewService = {
+
+    async getAll() {
+        return await instance<IReview[]>({
+            url: REVIEWS,
+            method: 'GET'
+        })
+    },
+
+    async createReview(productId: string | number, data: DataType) {
+        return await instance<IReview>({
+            url: `${REVIEWS}/leave/${productId}`,
+            method: 'POST',
+            data
+        })
+    },
+
+}
