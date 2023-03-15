23b7d822e6eb6007cec5126724d4d8ab0cb46619
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-03-15 22:50:49 +0300

    made products controller

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,7 +1,72 @@
-import { Controller } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { GetAllProductDto } from './dto/get-all.product.dto'
+import { ProductDto } from './dto/product.dto'
 import { ProductService } from './product.service'
 
 @Controller('products')
 export class ProductController {
 	constructor(private readonly productService: ProductService) {}
+
+	@UsePipes(new ValidationPipe())
+	@Get()
+	async getAll(@Query() queryDto: GetAllProductDto) {
+		return this.productService.getAll(queryDto)
+	}
+
+	@Get('similar/:id')
+	async getSimilar(@Param('id') id: string) {
+		return this.productService.getSimilar(+id)
+	}
+
+	@Get('by-slug/:slug')
+	async getProductsBySlug(@Param('slug') slug: string) {
+		return this.productService.bySlug(slug)
+	}
+
+	@Get('by-category/:categorySlug')
+	async getProductsByCategory(@Param('categorySlug') categorySlug: string) {
+		return this.productService.byCategory(categorySlug)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Post()
+	async createProduct() {
+		return this.productService.createProduct()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put(':id')
+	@Auth()
+	async updateProduct(@Param('id') id: string, @Body() dto: ProductDto) {
+		return this.productService.updateProduct(+id, dto)
+	}
+
+	@HttpCode(200)
+	@Delete(':id')
+	@Auth()
+	async deleteProduct(@Param('id') id: string) {
+		return this.productService.deleteProduct(+id)
+	}
+
+	@Get(':id')
+	@Auth()
+	async getProduct(@Param('id') id: string) {
+		return this.productService.byId(+id)
+	}
 }
