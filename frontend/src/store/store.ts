65e5abc9df8f20059c539e3180ae35bdbb44027f
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-03-16 04:31:25 +0300

    prepeare to make components architecture of project

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,7 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
-import { carouselSlice } from './carousel/carousel.slice'
+// import { carouselSlice } from './carousel/carousel.slice'
 import { cartSlice } from './cart/cart.slice'
 import { userSlice } from './user/user.slice';
 
@@ -14,7 +14,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     cart: cartSlice.reducer,
-    carousel: carouselSlice.reducer,
+    // carousel: carouselSlice.reducer,
     user: userSlice.reducer
 })
 
