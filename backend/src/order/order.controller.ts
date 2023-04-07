c827636620e2d8f6cb64944165f0ec8714611400
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-04-08 00:23:58 +0300

    some errors? I like it

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
 import { Auth } from 'src/auth/decorators/auth.decorator'
 import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { OrderDto } from './order.dto'
 import { OrderService } from './order.service'
 
 @Controller('orders')
@@ -11,5 +20,13 @@ export class OrderController {
 	@Auth()
 	getAll(@CurrentUser('id') userId: number) {
 		return this.orderService.getAll(userId)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post()
+	@Auth()
+	placeOrder(@Body() dto: OrderDto, @CurrentUser('id') userId: number) {
+		return this.orderService.placeOrder(dto, userId)
 	}
 }
--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
+import { productReturnObject } from 'src/product/return-product.object'
+import { OrderDto } from './order.dto'
 
 @Injectable()
 export class OrderService {
@@ -13,6 +15,33 @@ export class OrderService {
 			orderBy: {
 				createdAt: 'desc',
 			},
+			include: {
+				items: {
+					include: {
+						product: {
+							select: productReturnObject,
+						},
+					},
+				},
+			},
 		})
+	}
+
+	async placeOrder(dto: OrderDto, userId: number) {
+		const order = await this.prisma.order.create({
+			data: {
+				status: dto.status,
+				items: {
+					create: dto.items,
+				},
+			},
+			user: {
+				connect: {
+					id: userId,
+				},
+			},
+		})
+
+		return order
 	}
 }
